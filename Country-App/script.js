e8765af9442abeaadda2509e0c4a4fc7f1cb9e9e
Sasa Nedic <svetasmak0@gmail.com>
2024-02-20 17:50:33 +0100

    Create try block

---
+++ Country-App/script.js
@@ -0,0 +1,10 @@
+async function fetchCountry() {
+  try {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/region/europe`
+    );
+    const data = await response.json();
+    console.log(data);
+  } 
+}
+fetchCountry();
