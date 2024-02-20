894277b5f4b588d9fcff897ce3b7cf0cce6cd964
Sasa Nedic <svetasmak0@gmail.com>
2024-02-20 22:12:42 +0100

    Create p for name

--- Country-App/script.js
+++ Country-App/script.js
@@ -2,11 +2,16 @@ function setCountries(countries) {
   const parentEl = document.querySelector(".country__list");
   countries.forEach((country) => {
     const li = document.createElement("li");
+    console.log(countries);
 
     const img = document.createElement("img");
     img.classList.add("country__list__item");
     img.src = country.flags.svg;
     li.appendChild(img);
+
+    const name = document.createElement("p");
+    name.textContent = country.name.common;
+    li.appendChild(name);
 
     parentEl.appendChild(li);
   });
