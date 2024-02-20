e344cc34298cdf6f009b2c0fdf0e97f6ad129116
Sasa Nedic <svetasmak0@gmail.com>
2024-02-20 18:42:44 +0100

    Create setCountries function

--- Country-App/script.js
+++ Country-App/script.js
@@ -1,10 +1,12 @@
+function setCountries(countries) {}
+
 async function fetchCountry() {
   try {
     const response = await fetch(
       `https://restcountries.com/v3.1/region/europe`
     );
     const data = await response.json();
-    console.log(data);
+    setCountries(data);
   } catch (error) {
     console.log(error);
   }
