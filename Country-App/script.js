a90fad0e64cabed0ffbf9572fe75e8902c1c3805
Sasa Nedic <svetasmak0@gmail.com>
2024-02-27 18:02:08 +0100

    Learn pushState

--- Country-App/script.js
+++ Country-App/script.js
@@ -109,3 +109,13 @@ nextBtn.addEventListener("click", function () {
 // ONE MORE EXAMPLE WHEN WE CAN USE POPSTATE
 // we can use popstate when we want implement navigation on single page and we click on <a></a> (some link on page)
 // which uses JavaScript to dynamically load content we can update URL with (history.pushState, history.replaceState())
+// HISTORY.PUSHSTATE
+// THE PUSHSTATE HISTORY is method of the History interface adds an entry browser's session history stack.
+// The pushState() method adds a new state to the browser's history.
+// It's used to add a new entry to the history without actually loading a new page.
+// It's applied when you want to simulate navigation to a new page within a single-page application.
+// It allows you to change the URL in the browser's address bar without actually loading a new page.
+// Method structure: history.pushState(stateObj, title, url).
+// stateObj: An object representing the state you're adding to the history. This object can contain any data you want to associate with that state.
+// title: The title of the state. Some browsers don't use it, so it's usually an empty string ('').
+// url: The URL you want to set.
